25b81bcbb51bfc59bdb30194b384c397c51370d5
Mahmoud shahin (Codexline) <mahmoudshahin1111@gmail.com>
2022-08-27 19:04:10 +0200

    Build scripts (#6)
    * added build scripts
    
    * added deploy github actions script for deploy
    
    * added deploy github actions script for deploy
    
    * added build scripts

---
+++ scripts/build.js
@@ -0,0 +1,35 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const packageJson = require("../package.json");
+
+const FILE_HEADERS = `
+/*!
+* NumberToArabicWords (https://mahmoudshahin1111.github.io/numbers-to-arabic-words/)
+* Copyright ${new Date(
+  Date.now()
+).getFullYear()} The NumberToArabicWords Authors (https://github.com/mahmoudshahin1111/numbers-to-arabic-words/contributors)
+* Licensed under MIT (https://github.com/mahmoudshahin1111/numbers-to-arabic-words/blob/master/LICENSE)
+*/
+`;
+
+function execute() {
+  try {
+    console.log("Building .... 🙄");
+    execSync("npx webpack");
+    const indexPath = path.join(
+      __dirname,
+      "../",
+      packageJson.buildPath,
+      "index.js"
+    );
+    const indexFileContent = fs.readFileSync(indexPath, "utf8");
+    fs.writeFileSync(indexPath, FILE_HEADERS + "\n" + indexFileContent);
+    execSync(`npx prettier ${indexPath} --write`);
+    console.log("Builded");
+  } catch (e) {
+    console.log("Build Fails");
+  }
+}
+
+execute();
